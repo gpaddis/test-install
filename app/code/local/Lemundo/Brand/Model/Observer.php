f3e160c8b63c24332f6f58aebe5abac59981e31e
Gianpiero Addis <gianpiero.addis@gmail.com>
2017-11-11 13:34:12 +0100

    Set catalog_product_save_before event hook

--- app/code/local/Lemundo/Brand/Model/Observer.php
+++ app/code/local/Lemundo/Brand/Model/Observer.php
@@ -1,22 +1,26 @@
 <?php
 
 /**
-* This observer listens for a catalog_product_save_after event and appends
-* the brand name to the product name.
+* The observer listens for a catalog_product_save_before event and appends
+* the brand name to the product name each time a product is saved.
 */
 class Lemundo_Brand_Model_Observer
 {
     /**
      * Append the brand to the product name before it is persisted in the database.
      *
-     * @return void
+     * @param Varien_Event_Observer $observer
+     *
+     * @return Lemundo_Brand_Model_Observer
      */
-    public function appendBrandName($observer)
+    public function appendBrandName(Varien_Event_Observer $observer)
     {
-        $brandName = Mage::getStoreConfig('brand_options/testaufgabe/brand');
         $product = $observer->getEvent()->getProduct();
+        $productName = $product->getName();
+        $brandName = Mage::getStoreConfig('brand_options/testaufgabe/brand');
 
-        $newName = $product->getName() . $brandName;
-        $product->setName($newName);
+        $product->setName($productName . $brandName);
+
+        return $this;
     }
 }
